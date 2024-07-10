3f30bfabdb5a0cb7426192809740778a03866b49
dayeonkimm <ekdyd516@gmail.com>
2024-07-11 03:09:59 +0900

    wait_for_db 테스트

--- potatoes/core/tests.py
+++ potatoes/core/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from unittest.mock import patch
+from django.core.management import call_command
 
-# Create your tests here.
+@patch('django.db.utils.ConnectionHandler.__getitem__')
+class CommandsTests(SimpleTestCase):
+    
+    # DB가 준비되었을 때 wait_for_db 잘
+    def test_wait_for_db_ready(self):
+        patched_getitem.return_value = True
+        
+        call_command('wait_for_db')
+    
+    def test_wait_for_db_delay(self):
+        pass
