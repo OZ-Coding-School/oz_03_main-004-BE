581a3d981af69f66d69d30f92a990006cbd18fc4
youngkwangjoo <dudrknd1642@gmail.com>
2024-07-16 14:50:22 +0900

    <FEATURE> potatoes, todo file에 models.py 내용 추가

--- potatoes/potatoes/models.py
+++ potatoes/potatoes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings  
+from common.models import TimeStampedModel 
 
-# Create your models here.
+class Potato(TimeStampedModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    potato_type_id = models.SmallIntegerField()  # 감자 타입을 smallint로 직접 저장
+    potato_nickname = models.CharField(max_length=20, blank=True, null=True)
+    potato_level = models.SmallIntegerField(blank=True, null=True)
+    potato_exp = models.SmallIntegerField(blank=True, null=True)
+    potato_coin = models.SmallIntegerField(blank=True, null=True)
+    is_selected = models.BooleanField(blank=True, null=True)
+
+    def __str__(self):
+        return self.potato_nickname or "Unnamed Potato"
