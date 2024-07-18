dbde7b2083c23f5061cd3d3274eecb927da30858
youngkwangjoo <dudrknd1642@gmail.com>
2024-07-19 00:56:41 +0900

    <FEATURE> attendances views 추가. 출석 및 코인 증가 감소 기능 test 진행안함

--- potato_project/baekjoons/views.py
+++ potato_project/baekjoons/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
 
-# Create your views here.
+
+# solved.ac에서 api를 받아오는 함수, 더 필요할 시 추가 할 예정
+def get_boj_profile(userid):
+    url = f"https://solved.ac/api/v3/user/show?handle={userid}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "username": data["handle"],
+            "tier": data["tier"],
+            "solved_count": data["solvedCount"],
+            "rating": data["rating"],
+        }
+    else:
+        return None
+
+
+# 유저 아이디를 입력해서 solved에 개인 아이디 입력하는 함수
+def profile_view(request, userid):
+    profile = get_boj_profile(userid)
+    if profile:
+        return JsonResponse(profile)
+    else:
+        return JsonResponse({"error": "User not found or API error."}, status=404)
