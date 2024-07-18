a0c1db08a3d2933e10fe48fe931401bd472840e3
dayeonkimm <ekdyd516@gmail.com>
2024-07-18 10:58:32 +0900

    <DOCS> todos/models.py 수정
    - user -> user_id 로 수정

--- potato_project/todos/models.py
+++ potato_project/todos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings  # 유저 모델을 가져오기 위해 사용
+from common.models import TimeStampedModel  # TimeStampedModel을 가져오기 위해 사용
 
-# Create your models here.
+class Todo(TimeStampedModel):
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    task = models.CharField(max_length=50)
+    is_done = models.BooleanField(default=False)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.task
