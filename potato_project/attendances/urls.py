e3d2c52d4ada39d7fe8f370c0c6c367a54096f3c
NohSungwoo <shtjddn0817@gmail.com>
2024-07-21 04:25:44 +0900

    <FEAT>: UserStackCreate View 추가 유저가 추가한 StackList를 저장하는 View

--- potato_project/attendances/urls.py
+++ potato_project/attendances/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
+
 # url을 보는데 아래 토큰을 발급받는과정이 로그인 이후 출석을 하는데 필요한 과정이라고 나오는데
 # 잘모르겠어서 이건 다같이 봐주면 좋겟음..이게 로그인할때 받은 코드를 활용하는건지를 잘 모르겟슴
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
--- potato_project/stacks/views.py
+++ potato_project/stacks/views.py
@@ -1,13 +1,26 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framwork.response import Response
 
 from .models import Stack
 from .serializers import StackSerializer
 
 
-# 전체 스택 조회
-class StackList(APIView):
-    def get(self, request):
-        stacks = Stack.objects.all()
+# 유저 스택 리스트 조회
+class UserStackList(APIView):
+    def get(self, request, user_id):
+        stacks = Stack.objects.filter(user_id=user_id)
         serializer = StackSerializer(stacks, many=True)
         return Response(serializer.data)
+
+
+#
+class UserStackCreate(APIView):
+    def post(self, request, user_id):
+        stacks = Stack.objects.filter(user_id=user_id, data=request.data)
+        serializer = StackSerializer(stacks)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
