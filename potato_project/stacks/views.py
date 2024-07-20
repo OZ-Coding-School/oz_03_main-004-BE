06ecf124a2e756f907c281433fd8667f4a8ac84a
NohSungwoo <shtjddn0817@gmail.com>
2024-07-21 03:13:06 +0900

    <FEAT>: UserStackList 생성 유저가 가지고 있는 스택 리스트를 조회하는 views

--- potato_project/stacks/views.py
+++ potato_project/stacks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framwork.response import Response
 
-# Create your views here.
+from .models import Stack
+from .serializers import StackSerializer
+
+
+# 전체 스택 조회
+class StackList(APIView):
+    def get(self, request):
+        stacks = Stack.objects.all()
+        serializer = StackSerializer(stacks, many=True)
+        return Response(serializer.data)
