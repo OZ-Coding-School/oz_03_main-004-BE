bbd6f91edcab668e76f93ab8c84e49079b664e1d
youngkwangjoo <dudrknd1642@gmail.com>
2024-07-22 14:36:12 +0900

    충돌 해결중

--- potato_project/stacks/views.py
+++ potato_project/stacks/views.py
@@ -1,11 +1,17 @@
+<<<<<<< HEAD
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+=======
+from rest_framework.views import APIView
+from rest_framwork.response import Response
+>>>>>>> 7958cf4 (<FEAT>: UserStackList 생성)
 
 from .models import Stack
 from .serializers import StackSerializer
 
 
+<<<<<<< HEAD
 # 유저 스택 리스트 조회
 class UserStackList(APIView):
     def get(self, request, user_id):
@@ -24,3 +30,11 @@ class UserStackCreate(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+=======
+# 전체 스택 조회
+class StackList(APIView):
+    def get(self, request):
+        stacks = Stack.objects.all()
+        serializer = StackSerializer(stacks, many=True)
+        return Response(serializer.data)
+>>>>>>> 7958cf4 (<FEAT>: UserStackList 생성)
