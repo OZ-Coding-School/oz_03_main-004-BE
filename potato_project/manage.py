b65743c0cac6b4bc05fea0e366d2a490f050e2dd
NohSungwoo <shtjddn0817@gmail.com>
2024-07-23 02:00:11 +0900

    <FIX>: 서버가 실행될 때 Debugger를 계속 실행시켜줘야 하는 이슈 해결

--- potato_project/manage.py
+++ potato_project/manage.py
@@ -4,15 +4,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
-    # add bellow lines (10–12)
     if os.environ.get("RUN_MAIN") == "1":
         import debugpy
-
         debugpy.listen(("0.0.0.0", 3000))
-        print("Waiting for debugger attach...")
-        debugpy.wait_for_client()
-
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
