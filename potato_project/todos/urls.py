873688697cbcc4961f57548aae5a91b91c5b9401
youngkwangjoo <dudrknd1642@gmail.com>
2024-07-23 10:17:13 +0900

    <FEATURE> todos views.py 작성

---
+++ potato_project/todos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import TodoView
+
+urlpatterns = [
+    path('todos/', TodoView.as_view(), name='todo-list-create'),
+    path('todos/<int:todo_id>/', TodoView.as_view(), name='todo-detail'),
+]
