ab7f4d2bbf5b31130370885500cc72640fc27b9a
dayeonkimm <ekdyd516@gmail.com>
2024-07-24 00:28:58 +0900

    <FIX> stacks/url.py 수정

--- potato_project/stacks/urls.py
+++ potato_project/stacks/urls.py
@@ -2,8 +2,4 @@ from django.urls import path
 
 from . import views
 
-<<<<<<< HEAD
 urlpatterns = [path("", views.UserStackList.as_view(), name="stack_list")]
-=======
-urlpatterns = [path("", views.StackList.as_view(), name="stack_list")]
->>>>>>> develop
