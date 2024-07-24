329df8d3ca702f06e4ad544c4d58ed5c14f74d16
NohSungwoo <shtjddn0817@gmail.com>
2024-07-24 16:37:20 +0900

    <Fix>: date 변수 중복 삭제, 오타 수정

--- potato_project/githubs/models.py
+++ potato_project/githubs/models.py
@@ -7,7 +7,7 @@ from users.models import User
 class Github(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="User id")
     commit_num = models.BigIntegerField(verbose_name="Commit Number")
-    date = date = models.DateTimeField()
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.github_id  # GitHub ID를 반환
--- potato_project/stacks/views.py
+++ potato_project/stacks/views.py
@@ -3,7 +3,7 @@ from django.db import DatabaseError
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framwork.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError
 
 from .models import Stack
 from .serializers import StackSerializer
