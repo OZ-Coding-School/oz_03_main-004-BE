9fe805801ae6113b508233ecebdb7fa5da2ea04b
youngkwangjoo <dudrknd1642@gmail.com>
2024-07-25 16:02:20 +0900

    <FIX> import 수정

--- potato_project/todos/urls.py
+++ potato_project/todos/urls.py
@@ -1,17 +1,12 @@
+# urls.py
 from django.urls import path
-from .views import TodoView
+from . import views
+
 
 urlpatterns = [
-    path("", TodoView.as_view(), name="todo_list"),  # GET 요청: /todos/
-    path(
-        "<int:todo_id>/", TodoView.as_view(), name="todo_detail"
-    ),  # GET 요청: /todos/<todo_id>/
-    path(
-        "completion_percentage/", TodoView.as_view(), name="completion_percentage_all"
-    ),  # GET 요청: /todos/completion_percentage/
-    path(
-        "completion_percentage/<int:user_id>/",
-        TodoView.as_view(),
-        name="completion_percentage_user",
-    ),  # GET 요청: /todos/completion_percentage/<user_id>/
+    path('', views.TodayTodoListView.as_view(), name='today_todo_list'),
+    path('list/', views.TodoListCreateView.as_view(), name='todo_list_create'),
+    path('detail/<int:pk>/', views.TodoRetrieveUpdateDestroyView.as_view(), name='todo_detail'),
+    path('toggle/<int:pk>/', views.TodoToggleView.as_view(), name='todo_toggle'),
+    path('monthly-rate/<int:year>/<int:month>/', views.MonthlyCompletionRateView.as_view(), name='monthly_completion_rate'),
 ]
