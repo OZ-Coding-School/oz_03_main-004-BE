1279070b3a52d2ae398c43dd29b7ea7b0244d855
dayeonkimm <ekdyd516@gmail.com>
2024-07-25 17:39:00 +0900

    <REFACTOR> stacks/urls.py 수정

--- potato_project/stacks/urls.py
+++ potato_project/stacks/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.UserStackList.as_view(), name="stack_list")]
+urlpatterns = [path("", views.StackList.as_view(), name="stack_list")]
