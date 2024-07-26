b33ea9195e4d4012b14bf9a7d1a55bf8a628f5e4
dayeonkimm <ekdyd516@gmail.com>
2024-07-26 23:34:40 +0900

    <FEAT> 닉네임필드 추가
    - 유저 테이블에 닉네임 필드 추가
    - default는 깃허브 유저네임
    - 추후 수정 가능한 api 생성

--- potato_project/users/serializers.py
+++ potato_project/users/serializers.py
@@ -6,4 +6,11 @@ from .models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ["user_id", "username", "profile_url", "github_id", "baekjoon_id"]
+        fields = [
+            "user_id",
+            "username",
+            "profile_url",
+            "github_id",
+            "baekjoon_id",
+            "nickname",
+        ]
