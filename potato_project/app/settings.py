e42ed1d5b93ee6e61f27e2d901b399edb0ef620d
NohSungwoo <shtjddn0817@gmail.com>
2024-07-28 03:40:47 +0900

    <CHORE>: User 모델에 email Field 추가

--- potato_project/app/settings.py
+++ potato_project/app/settings.py
@@ -40,17 +40,20 @@ CUSTOM_USER_APPS = [
     "stacks.apps.StacksConfig",
     "user_stacks.apps.UserStacksConfig",
     "todos.apps.TodosConfig",
+    
     # django-rest-framework
     "rest_framework",
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    
     # django-rest-auth
     "dj_rest_auth",
     "dj_rest_auth.registration",
+
     # django-allauth
-    # "allauth",
-    # "allauth.account",
+    "allauth",
+    "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.github",
 ]
