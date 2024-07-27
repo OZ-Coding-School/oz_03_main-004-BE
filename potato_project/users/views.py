bc5c88cfe104b5961c2a5caa509bf3ed46edf075
NohSungwoo <shtjddn0817@gmail.com>
2024-07-28 03:51:41 +0900

    <REFACTOR>: black, isort

--- potato_project/users/views.py
+++ potato_project/users/views.py
@@ -12,7 +12,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import DatabaseError, IntegrityError
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect
-
 # .env 파일에서 환경 변수 로드 (python-dotenv 라이브러리 필요)
 from dotenv import load_dotenv
 from rest_framework import status
