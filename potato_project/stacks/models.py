d35454b2945f37d3c658185eed983604a3495676
youngkwangjoo <dudrknd1642@gmail.com>
2024-07-29 18:11:50 +0900

    <FIX> stacks models.py 길이수정

--- potato_project/stacks/models.py
+++ potato_project/stacks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Stack(models.Model):
-    name = models.CharField(max_length=20, null=True, verbose_name="스택명")
+    name = models.CharField(max_length=50, null=True, verbose_name="스택명")
 
     def __str__(self):
         return self.name
