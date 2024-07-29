c4c56a65a1199d5c95e01e69f9bc342bd3d3aab0
dayeonkimm <ekdyd516@gmail.com>
2024-07-30 03:58:47 +0900

    <REFACTOR> Potato 모델 수정
    - user는 순환참조
    - potato_type은 순환참조 x

--- potato_project/potatoes/models.py
+++ potato_project/potatoes/models.py
@@ -1,13 +1,12 @@
 from common.models import TimeStampedModel
 from django.db import models
+from potato_types.models import PotatoType
 
 
 class Potato(TimeStampedModel):
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, null=True, blank=True
     )
-    potato_type_id = models.ForeignKey(
-        "potato_type.PotatoType", on_delete=models.CASCADE
-    )
+    potato_type = models.ForeignKey(PotatoType, on_delete=models.CASCADE)
     is_acquired = models.BooleanField(blank=True, null=True)
     is_selected = models.BooleanField(blank=True, null=True)
