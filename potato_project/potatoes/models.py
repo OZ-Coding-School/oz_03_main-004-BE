58d88aea67671ff7b136e688b293f259520a98a9
dayeonkimm <ekdyd516@gmail.com>
2024-07-29 20:41:40 +0900

    <REFACTOR> 순환참조 오류

--- potato_project/potatoes/models.py
+++ potato_project/potatoes/models.py
@@ -1,12 +1,13 @@
 from common.models import TimeStampedModel
 from django.db import models
-from potato_types.models import PotatoType
-from users.models import User
 
 
 class Potato(TimeStampedModel):
-    # id제거
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    potato_type_id = models.ForeignKey(PotatoType, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, null=True, blank=True
+    )
+    potato_type_id = models.ForeignKey(
+        "potato_type.PotatoType", on_delete=models.CASCADE
+    )
     is_acquired = models.BooleanField(blank=True, null=True)
     is_selected = models.BooleanField(blank=True, null=True)
