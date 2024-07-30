b6cf6126f79f06ae990c6f828e4ea4af83cd56be
NohSungwoo <shtjddn0817@gmail.com>
2024-07-31 01:44:56 +0900

    <FEAT>: potatoes/views/MyPotatoDetail 테스트 코드 추가

--- potato_project/app/settings.py
+++ potato_project/app/settings.py
@@ -118,7 +118,9 @@ DATABASES = {
 
 # 비밀번호 검증 설정
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
