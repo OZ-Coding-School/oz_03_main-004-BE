ef5399a0eadae0aa5cfc17d8ca6b01be63a03b50
NohSungwoo <shtjddn0817@gmail.com>
2024-07-31 02:48:01 +0900

    <FEAT>: StackListTest 추가 test_get_stack_list_success함수 생성

--- potato_project/githubs/tests.py
+++ potato_project/githubs/tests.py
@@ -6,16 +6,18 @@ from .views import GitHubAPIService
 
 class TestGitHubAPIService(unittest.TestCase):
     def setUp(self):
-        self.access_token = 'fake_access_token'
-        self.github_service = GitHubAPIService(self.access_token) # 생성자가 있는 경우에 인자 값 전달 가능
-        self.repo = 'kingtato/HelloFlower'
-        self.username = 'kingtato'
-    
+        self.access_token = "fake_access_token"
+        self.github_service = GitHubAPIService(
+            self.access_token
+        )  # 생성자가 있는 경우에 인자 값 전달 가능
+        self.repo = "kingtato/HelloFlower"
+        self.username = "kingtato"
+
     # @patch 문법은 unittest.mock 모듈에서 지원하는 문법
     # request.get을 모킹한다.
     # 커밋을 얻어오는 지 테스트한다.
-    @patch('.views.requests.get')
-    def test_get_commits(self, mock_get): # 데코레이터로 생성한 Mock 객체를 mock_get 
+    @patch(".views.requests.get")
+    def test_get_commits(self, mock_get):  # 데코레이터로 생성한 Mock 객체를 mock_get
         mock_response = mock_get.return_value
         mock_response.status_code = 200
         mock_response.json.return_value = [{"commit": "data"}]
@@ -28,10 +30,10 @@ class TestGitHubAPIService(unittest.TestCase):
             headers={
                 "Authorization": f"token {self.access_token}",
                 "Accept": "application/vnd.github.v3+json",
-            }
+            },
         )
 
-    @patch('mymodule.requests.get')
+    @patch("mymodule.requests.get")
     def test_get_repos(self, mock_get):
         # 설정
         mock_response = mock_get.return_value
@@ -48,10 +50,10 @@ class TestGitHubAPIService(unittest.TestCase):
             headers={
                 "Authorization": f"token {self.access_token}",
                 "Accept": "application/vnd.github.v3+json",
-            }
+            },
         )
 
-    @patch('mymodule.requests.get')
+    @patch("mymodule.requests.get")
     def test_get_total_commits(self, mock_get):
         # 설정
         mock_response = mock_get.return_value
@@ -68,10 +70,10 @@ class TestGitHubAPIService(unittest.TestCase):
             headers={
                 "Authorization": f"token {self.access_token}",
                 "Accept": "application/vnd.github.v3+json",
-            }
+            },
         )
 
-    @patch('mymodule.requests.get')
+    @patch("mymodule.requests.get")
     def test_get_commits_error(self, mock_get):
         # 설정
         mock_response = mock_get.return_value
@@ -84,7 +86,7 @@ class TestGitHubAPIService(unittest.TestCase):
         self.assertIsNone(commits)
         self.assertEqual(status_code, 404)
 
-    @patch('mymodule.requests.get')
+    @patch("mymodule.requests.get")
     def test_get_repos_error(self, mock_get):
         # 설정
         mock_response = mock_get.return_value
@@ -97,19 +99,21 @@ class TestGitHubAPIService(unittest.TestCase):
         self.assertIsNone(repos)
         self.assertEqual(status_code, 404)
 
-    @patch('mymodule.requests.get')
+    @patch("mymodule.requests.get")
     def test_get_total_commits_error(self, mock_get):
         # 설정
         mock_response = mock_get.return_value
         mock_response.status_code = 404
 
         # 실행
-        total_commits, status_code = self.github_service.get_total_commits(self.username)
+        total_commits, status_code = self.github_service.get_total_commits(
+            self.username
+        )
 
         # 검증
         self.assertIsNone(total_commits)
         self.assertEqual(status_code, 404)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- potato_project/stacks/tests.py
+++ potato_project/stacks/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from stacks.models import Stack
+from stacks.serializers import StackSerializer
+from users.models import User
 
-# Create your tests here.
+
+class StackListTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username="kingtato", nickname="kingtato")
+        self.client.force_authenticate(user=self.user)
+
+        self.stack1 = Stack.objects.create(name="Python")
+        self.stack2 = Stack.objects.create(name="Java")
+
+        self.url = "/stacks/all"
+
+    def test_get_stack_list_success(self):
+        response = self.client.get(self.url)
+
+        # 응답 코드 확인
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # 응답 데이터 확인
+        response_data = response.json()
+        expected_data = StackSerializer([self.stack1, self.stack2], many=True).data
+        self.assertEqual(response_data, expected_data)
