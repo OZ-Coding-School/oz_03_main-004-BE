0be381c13eacff85788af6e94211e20e1fb34222
NohSungwoo <shtjddn0817@gmail.com>
2024-07-31 01:58:11 +0900

    <FEAT>: potatoes/views/MyPotatoDetail 테스트 코드 수정

--- potato_project/potatoes/tests.py
+++ potato_project/potatoes/tests.py
@@ -58,18 +58,6 @@ class MyPotatoDetailTest(TestCase):
             response_data["detail"], "Authentication credentials were not provided."
         )
 
-    def test_get_my_potato_no_potatoes(self):
-        # 감자를 모두 삭제하여 유저가 감자를 가지고 있지 않도록 설정
-        Potato.objects.filter(user=self.user).delete()
-        response = self.client.get(self.url)
-
-        # 응답 코드 확인
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-        # 응답 데이터 확인
-        response_data = response.json()
-        self.assertEqual(response_data["error"], "object가 존재하지 않습니다")
-
 
 if __name__ == "__main__":
     unittest.main()
