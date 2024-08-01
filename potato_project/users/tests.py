623896aceb569a051b890af43e898b0dd654a672
NohSungwoo <shtjddn0817@gmail.com>
2024-08-01 23:54:22 +0900

    <FEAT>: views.UserDetail 테스트 코드 추가

--- potato_project/users/tests.py
+++ potato_project/users/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import RequestFactory, TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase, force_authenticate
+from users.models import User
+from users.views import UserDetail
 
-# Create your tests here.
+
+class UserDetailTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username="testuser", password="password", nickname="testpotato"
+        )
+
+    def test_get_user_profile(self):
+        request = self.factory.get("/user/profile/")
+        force_authenticate(request, user=self.user)
+        response = UserDetail.as_view()(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["username"], self.user.username)
+        self.assertEqual(response.data["nickname"], self.user.nickname)
