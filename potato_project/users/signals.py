1576df972349736aa81abae2cbb067a43c9a9bf1
dayeonkimm <ekdyd516@gmail.com>
2024-09-20 18:03:42 +0900

    오류 수정

--- potato_project/users/signals.py
+++ potato_project/users/signals.py
@@ -5,17 +5,21 @@ from users.models import User
 
 
 @receiver(post_save, sender=User)
-def get_level_two_potato(sender, instance, **kwargs):
+def get_level_two_potato(sender, instance, created, **kwargs):
+    if created:
+        return
+
+    # 이전 인스턴스 재조회
+    previous_instance = User.objects.get(pk=instance.pk)
+
     # 레벨이 2로 변경되었고, 이전 레벨이 2가 아닐 때만 실행
     if (
-        instance.potato_level == 2
-        and instance.tracker.has_changed('potato_level')
-        and instance.tracker.previous("potato_level") != 2
-        and not kwargs.get("created", False)
+        previous_instance.potato_level != instance.potato_level
+        and instance.potato_level == 2
     ):
-      	try:
+        try:
             # potato_type_id=2인 감자 조회
-            potato = Potato.objects.get(user=instance.user, potato_type_id=2)
+            potato = Potato.objects.get(user=instance, potato_type_id=2)
             if not potato.is_acquired:
                 potato.is_acquired = True
                 potato.save()
@@ -24,17 +28,22 @@ def get_level_two_potato(sender, instance, **kwargs):
             pass
 
 
-
 @receiver(post_save, sender=User)
-def get_level_three_potato(sender, instance, **kwargs):
+def get_level_three_potato(sender, instance, created, **kwargs):
+    if created:
+        return
+
+    # 이전 인스턴스 재조회
+    previous_instance = User.objects.get(pk=instance.pk)
+
+    # 레벨이 3로 변경되었고, 이전 레벨이 3가 아닐 때만 실행
     if (
-        instance.potato_level == 3
-        and instance.tracker.has_changed('potato_level')
-        and instance.tracker.previous("potato_level") != 3
-        and not kwargs.get("created", False)
+        previous_instance.potato_level != instance.potato_level
+        and instance.potato_level == 3
     ):
         try:
-            potato = Potato.objects.get(user=instance.user, potato_type_id=3)
+            # potato_type_id=3인 감자 조회
+            potato = Potato.objects.get(user=instance, potato_type_id=3)
             if not potato.is_acquired:
                 potato.is_acquired = True
                 potato.save()
@@ -43,36 +52,46 @@ def get_level_three_potato(sender, instance, **kwargs):
 
 
 @receiver(post_save, sender=User)
-def get_level_four_potato(sender, instance, **kwargs):
+def get_level_four_potato(sender, instance, created, **kwargs):
+    if created:
+        return
+
+    # 이전 인스턴스 재조회
+    previous_instance = User.objects.get(pk=instance.pk)
+
+    # 레벨이 4로 변경되었고, 이전 레벨이 4가 아닐 때만 실행
     if (
-        instance.potato_level == 4
-        and instance.tracker.previous("potato_level") != 4
-        and instance.tracker.has_changed('potato_level')
-        and not kwargs.get("created", False)
+        previous_instance.potato_level != instance.potato_level
+        and instance.potato_level == 4
     ):
-      	try:
-            potato = Potato.objects.get(user=instance.user, potato_type_id=4)
+        try:
+            # potato_type_id=4인 감자 조회
+            potato = Potato.objects.get(user=instance, potato_type_id=4)
             if not potato.is_acquired:
                 potato.is_acquired = True
                 potato.save()
         except Potato.DoesNotExist:
             pass
-
 
 
 @receiver(post_save, sender=User)
-def get_level_five_potato(sender, instance, **kwargs):
+def get_level_five_potato(sender, instance, created, **kwargs):
+    if created:
+        return
+
+    # 이전 인스턴스 재조회
+    previous_instance = User.objects.get(pk=instance.pk)
+
+    # 레벨이 5로 변경되었고, 이전 레벨이 5가 아닐 때만 실행
     if (
-        instance.potato_level == 5
-        and instance.tracker.has_changed('potato_level')
-        and instance.tracker.previous("potato_level") != 5
-        and not kwargs.get("created", False)
+        previous_instance.potato_level != instance.potato_level
+        and instance.potato_level == 5
     ):
-      	try:
-            potato = Potato.objects.get(user=instance.user, potato_type_id=5)
+        try:
+            # potato_type_id=5인 감자 조회
+            potato = Potato.objects.get(user=instance, potato_type_id=5)
             if not potato.is_acquired:
                 potato.is_acquired = True
                 potato.save()
         except Potato.DoesNotExist:
             pass
-
